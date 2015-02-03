41bda6c38c3f8535997bb2f5321b474e9cb270c7
Kishore <seemalakishore@yahoo.in>
2015-02-03 15:33:07 +0530

    Kishore - web services

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -10,7 +10,7 @@ totalPowerValue = 0
 
 s.cron '0 1 * * *' do  # for every hour
   
-  session.execute("USE enos_#{Account.first.company_reference}")   
+  session.execute("USE enos_sample")   
   Circuit.where(:active => true).each do |circuit|
     sum = 0
     results = session.execute("select * from emon_hourly_data where panel='#{circuit.panel.equip_ref}' and channel='#{circuit.channel_no}' and asof_hr>=#{time} and asof_hr<#{time.end_of_day.to_i}")
